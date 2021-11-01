366236ee7d2b0640c813ca34b09a891b9e56ac82
era <nathan.lerue@gmail.com>
2021-11-01 16:37:56 -0300

    pretty print server script

--- main.ts
+++ main.ts
@@ -14,87 +14,114 @@
    limitations under the License.
 */
 
-import * as chalk from 'chalk';
-import express, { json } from 'express';
-import fs from 'fs'
+import * as chalk from "chalk";
+import express, { json } from "express";
+import fs from "fs";
 
-import * as consts from './src/consts';
-import {GetData} from './src/GetData';
-import log from './tools/log';
+import * as consts from "./src/consts";
+import { GetData } from "./src/GetData";
+import log from "./tools/log";
 
 // initiate the app instance of express
 const app = express();
 
 // init the data object to hold all covid data, and run function GetData to update it each hour (*usually*)
 var data: any;
-(async()=>{
-    data = await GetData();
-    log("Data updated", true)
-})()
+(async () => {
+  data = await GetData();
+  log("Data updated", true);
+})();
 
-app.get('/api/internal/data/lang', async (req, res) => {
-    let lang = req.query.lang as String;
-    fs.readFile(__dirname + `/data/${lang}.jsonc`, (err, data) => {
-        if(err) return res.status(500).send("Internal server error. Pak is either not available or could not be found.")
-        else {
-            res.status(200).send(JSON.stringify(data));
-        }
-    })
-})
-
-app.get('/api/internal/overview', async (req, res) => {
-    // search to see if data has the country code the request is asking for.
-    var countrycode = decodeURIComponent((req.query.cc as string).toString());
-    // if cc is null, send a 400 error response.
-    if(!countrycode) {
-        return res.status(400).send(`Bad request. (Query param cc [?cc=] is null)`);
+app.get("/api/internal/data/lang", async (req, res) => {
+  let lang = req.query.lang as String;
+  fs.readFile(__dirname + `/data/${lang}.jsonc`, (err, data) => {
+    if (err)
+      return res
+        .status(500)
+        .send(
+          "Internal server error. Pak is either not available or could not be found."
+        );
+    else {
+      res.status(200).send(JSON.stringify(data));
     }
-    // object that should be returned later
-    var returnObj: any = new Object();
-    
-    // check if data has country code.
-    let country_data: any = data[countrycode.toUpperCase()];
+  });
+});
 
-    (!country_data) ?? res.status(400).send(`Invalid country code ${country_data}. Please make sure that this country is supported by CoDash, and that the code is valid.`)
+app.get("/api/internal/overview", async (req, res) => {
+  // search to see if data has the country code the request is asking for.
+  var countrycode = decodeURIComponent((req.query.cc as string).toString());
+  // if cc is null, send a 400 error response.
+  if (!countrycode) {
+    return res.status(400).send(`Bad request. (Query param cc [?cc=] is null)`);
+  }
+  // object that should be returned later
+  var returnObj: any = new Object();
 
-    // get current date in format of json
-    let today = (new Date().getFullYear()) + '-' + ((new Date().getMonth()+1).toString()) + '-' + (new Date().getDate()).toString();
-    let yesterday = (new Date().getFullYear().toString()) + '-' + ((new Date().getMonth()+1).toString()) + '-' + ((new Date().getDate() - 1)).toString();
-    let twodays = (new Date().getFullYear()) + '-' + ((new Date().getMonth()+1).toString()) + '-' + (new Date().getDate() - 2).toString();
+  // check if data has country code.
+  let country_data: any = data[countrycode.toUpperCase()];
 
-    /**********************
-     * HOW THIS WORKS
-     * The filter method works by searching all of the data in the requested country code.
-     * If the data for today isn't available, it should treat yesterday as today.
-     * So, lets say that 2021-10-31 isn't available, but 2021-10-30 is.
-     * The filter would then treat the two days as 2021-10-30 and 2021-10-29.
-    */
+  !country_data ??
+    res
+      .status(400)
+      .send(
+        `Invalid country code ${country_data}. Please make sure that this country is supported by CoDash, and that the code is valid.`
+      );
 
-    // today filter method
-    let today_filter = country_data.data.filter((obj: { date: string; }) => {
-        // fallback to yesterday if today isn't available.
-        return obj.date == today || yesterday;
-    })
-    // yesterday filter method.
-    let yesterday_filter = country_data.data.filter((obj: { date: string; }) => {
-        // fallback to two days ago if todays data is not available.
-        return (today_filter.date == today) ? obj.date == yesterday : obj.date == twodays;
-    })
+  // get current date in format of json
+  let today =
+    new Date().getFullYear() +
+    "-" +
+    (new Date().getMonth() + 1).toString() +
+    "-" +
+    new Date().getDate().toString();
+  let yesterday =
+    new Date().getFullYear().toString() +
+    "-" +
+    (new Date().getMonth() + 1).toString() +
+    "-" +
+    (new Date().getDate() - 1).toString();
+  let twodays =
+    new Date().getFullYear() +
+    "-" +
+    (new Date().getMonth() + 1).toString() +
+    "-" +
+    (new Date().getDate() - 2).toString();
 
-    // bind the case stats to the return object.
-    returnObj["stats"] = {
-        today: today_filter[0],
-        yesterday: yesterday_filter[0]
-    };
-    
-    // free up some memory by deleting unused objects.
-    delete yesterday_filter[0], today_filter[0];
+  /**********************
+   * HOW THIS WORKS
+   * The filter method works by searching all of the data in the requested country code.
+   * If the data for today isn't available, it should treat yesterday as today.
+   * So, lets say that 2021-10-31 isn't available, but 2021-10-30 is.
+   * The filter would then treat the two days as 2021-10-30 and 2021-10-29.
+   */
 
-    // send the data.
+  // today filter method
+  let today_filter = country_data.data.filter((obj: { date: string }) => {
+    // fallback to yesterday if today isn't available.
+    return obj.date == today || yesterday;
+  });
+  // yesterday filter method.
+  let yesterday_filter = country_data.data.filter((obj: { date: string }) => {
+    // fallback to two days ago if todays data is not available.
+    return today_filter.date == today
+      ? obj.date == yesterday
+      : obj.date == twodays;
+  });
 
-    return res.status(200).send(JSON.stringify(returnObj));
-})
+  // bind the case stats to the return object.
+  returnObj["stats"] = {
+    today: today_filter[0],
+    yesterday: yesterday_filter[0],
+  };
+
+  // free up some memory by deleting unused objects.
+  delete yesterday_filter[0], today_filter[0];
+
+  // send the data.
+
+  return res.status(200).send(JSON.stringify(returnObj));
+});
 
 app.listen(80, () => {
-    log("Server started on port 80", true)
-})
+  log("Server started on port 80", true);
+});
